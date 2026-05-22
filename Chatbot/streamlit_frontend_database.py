ba9031ddf4be3414fc6191e6c784ec023f3fe901
deepak <deepak.pandey@thesqua.re>
2026-05-22 14:28:34 +0530

    added a CONFIG and modify old CONFIG to trace the chatbot conversaton using LANGSMITH just updating .env

--- Chatbot/streamlit_frontend_database.py
+++ Chatbot/streamlit_frontend_database.py
@@ -36,7 +36,16 @@ if 'chat_threads' not in st.session_state:
 
 add_thread(st.session_state['thread_id'])
 
-CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
+# CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
+
+#we added this new config for LAngSmithing proper tracing based on thread_id
+CONFIG = {
+    "configurable": {"thread_id": st.session_state["thread_id"]},
+    "metadata": {
+        "thread_id": st.session_state["thread_id"]
+    },
+    "run_name": "chat_turn",
+}
 
 #---------sidebar UI
 st.sidebar.title('Chatbot')
